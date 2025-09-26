a46ee3754d430e14e97f957dc6e0f72449fc5a6f
tahmid1406 <tahmidpranto1406@gmail.com>
2025-09-26 02:37:06 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Academics from './pages/Academics';
+import Publications from './pages/Publications';
+import Skills from './pages/Skills';
+import Experience from './pages/Experience';
+import Projects from './pages/Projects';
+import Certification from './pages/Certification';
+import Thesis from './pages/Thesis';
+import Blog from './pages/Blog';
+import './style.css';
 
 function App() {
+  const [activeSection, setActiveSection] = useState('home');
+
+  const renderPage = () => {
+    switch (activeSection) {
+      case 'home':
+        return <Home />;
+      case 'academics':
+        return <Academics />;
+      case 'publications':
+        return <Publications />;
+      case 'skills':
+        return <Skills />;
+      case 'experience':
+        return <Experience />;
+      case 'projects':
+        return <Projects />;
+      case 'certifications':
+        return <Certification />;
+      case 'thesis':
+        return <Thesis />;
+      case 'blog':
+        return <Blog />;
+      default:
+        return <Home />;
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar activeSection={activeSection} setActiveSection={setActiveSection} />
+      <main className="main-content">
+        {renderPage()}
+      </main>
     </div>
   );
 }
